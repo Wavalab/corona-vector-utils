0c7f351bc521683a9cca09a53ebae6dab6e448fa
John Chin-Jew <jchinjew@gmail.com>
2017-07-25 00:33:34 -0700

    Add files via upload

---
+++ vector-utils.lua
@@ -0,0 +1,22 @@
+--
+-- VECTORS
+--
+-- hypotenuse of legs x and y of a right triangle
+utils.hypo = function(x, y)
+  return math.sqrt(x*x+y*y)
+end
+
+-- normalize vector given x and y components; optional scalar
+utils.normalize = function(x, y, scalar)
+  local m = utils.hypo(x, y)
+  if scalar == nil then
+    return x/m, y/m
+  end
+  return (x/m)*scalar, (y/m)*scalar
+end
+
+-- returns unit vector of two points optionally multiplied by scalar
+utils.vectorize = function(x1, y1, x2, y2, scalar)
+  local x, y = x2-x1, y2-y1
+  return utils.normalize(x, y, scalar)
+end
